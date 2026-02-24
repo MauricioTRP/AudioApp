3889fada031a0c3b32570a9aff2d6181a37e3aa3
mfuent <mauriciofb@gmail.com>
2026-02-24 16:52:20 -0300

    added oboe to cmake file

---
+++ audio_management/src/main/cpp/AudioWrapper.h
@@ -0,0 +1,15 @@
+//
+// Created by mfuent on 2/24/2026.
+//
+
+#ifndef AUDIOAPP_AUDIOWRAPPER_H
+#define AUDIOAPP_AUDIOWRAPPER_H
+
+#include <oboe/Oboe.h>
+
+class AudioWrapper {
+    oboe::AudioStream* mStream;
+};
+
+
+#endif //AUDIOAPP_AUDIOWRAPPER_H
---
+++ audio_management/src/main/cpp/AudioWrapper.mm
@@ -0,0 +1,5 @@
+//
+// Created by mfuent on 2/24/2026.
+//
+
+#include "AudioWrapper.h"
